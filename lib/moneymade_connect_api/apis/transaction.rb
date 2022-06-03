b2740224b3caf1d46054304c511df798871c20b3
v_pavelko <shadows0breath@gmail.com>
2022-06-03 15:28:14 +0300

    remove ticker parameter

--- lib/moneymade_connect_api/apis/transaction.rb
+++ lib/moneymade_connect_api/apis/transaction.rb
@@ -3,16 +3,11 @@ require_relative './api.rb'
 class TransactionAPI < API
   def get_list(user_id, account_id, queryParams = {})
     $params = {
-        ticker: queryParams.fetch(:ticker, nil),
         limit: queryParams.fetch(:limit, 1),
         offset: queryParams.fetch(:offset, 0),
     };
 
     $url = "api/v1/users/#{user_id}/accounts/#{account_id}/transactions?limit=#{$params[:limit]}&offset=#{$params[:offset]}"
-
-    if $params[:ticker] != nil
-      $url = $url + "&ticker=#{$params[:ticker]}"
-    end
     
     return @client.request({
       method: 'GET',
